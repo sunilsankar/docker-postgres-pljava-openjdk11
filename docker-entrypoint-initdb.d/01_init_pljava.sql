072d1d872e9d231b59c981b1ddafc2ea0c4bfba9
Sunil Sankar <sunil@sunil.cc>
2022-06-19 06:55:12 +0200

    added the new structure

--- docker-entrypoint-initdb.d/01_init_pljava.sql
+++ docker-entrypoint-initdb.d/01_init_pljava.sql
@@ -1,4 +1,4 @@
-SET pljava.libjvm_location TO '/usr/lib/jvm/java-11-openjdk-amd64/lib/server/libjvm.so';
+SET pljava.libjvm_location TO '/usr/lib/jvm/java-11-openjdk/lib/server/libjvm.so';
 ALTER DATABASE postgres SET pljava.libjvm_location FROM CURRENT;
 ALTER USER postgres SET SEARCH_PATH TO public,sqlj;
 CREATE EXTENSION pljava;
